3d963eb768400140d6f187f68e9d67d4e841e143
melissa <nadia.mm.mariduena@gmail.com>
2020-12-18 18:48:57 +0100

    animation process preview| animated text inside the nav open

--- src/components/Hamburger.js
+++ src/components/Hamburger.js
@@ -23,6 +23,7 @@ const Hamburger = ({ state }) => {
   let line1 = useRef(null);
   let line2 = useRef(null);
   let line3 = useRef(null);
+  //
   let info = useRef(null);
   // after declaring the refs, assign them in the jsx/dom to all:  ref={(el) => (menu = el)}
   // the "el / elements" that are going to be animate
@@ -72,14 +73,18 @@ const Hamburger = ({ state }) => {
       });
       //---------------------
       staggerReveal(revealMenu);
+      fadeInUp(info);
+      staggerText(line1, line2, line3);
       //---------------------
     }
-  }, [state]);
+  }, [state]); //we need to wait the state to be finish or has change
+  //  before it runs the useEffect, he used this because without the effect of the
+  // skew is a bit broken when the dropdown reaches the 100%
   //
   //
   // type: sfc snippet: const  = () => {
   const staggerReveal = (node1) => {
-    gsap.from([node1], {
+    gsap.from(node1, {
       duration: 0.8,
       height: 0,
       transformOrigin: "right top", // will slightly rotate the nav with the help of the skew: 2
@@ -88,6 +93,34 @@ const Hamburger = ({ state }) => {
       stagger: {
         amount: 0.1,
       },
+    });
+  };
+  //
+  // line 1, 2 ,3
+  //
+  const staggerText = (node1, node2, node3) => {
+    gsap.from([node1, node2, node3], {
+      duration: 1.5,
+      y: 100,
+      delay: 0.1,
+      ease: "power3.inOut",
+      stagger: {
+        amount: 0.9, //each line will take a bit time to show, 
+        // if you add 0.1 it will look like all the 3 lines come 
+        // at the same time, that s why i dont like it
+      },
+    });
+  };
+  //
+  // paragraph 'info'
+  //
+  const fadeInUp = (node) => {
+    gsap.from(node, {
+      y: 60,
+      duration: 1.5,
+      delay: 0.2,
+      opacity: 0,
+      ease: "power3.inOut",
     });
   };
 
@@ -143,3 +176,5 @@ const Hamburger = ({ state }) => {
 };
 
 export default Hamburger;
+
+
